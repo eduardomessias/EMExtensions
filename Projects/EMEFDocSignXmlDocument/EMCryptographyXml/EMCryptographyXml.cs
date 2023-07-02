e7158f985fea8a442f8168448e4ada7c46cd8d99
Eduardo Messias <eduardo.messias@outlook.com>
2023-07-02 19:39:47 +0000

    Sign document with Id

---
+++ Projects/EMEFDocSignXmlDocument/EMCryptographyXml/EMCryptographyXml.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.Xml;
+using System.Xml;
+
+namespace EMCryptographyXml
+{
+    public class SignedXmlWithId : SignedXml
+    {
+        public override XmlElement GetIdElement(XmlDocument document, string idValue)
+        {
+            if (!(document.SelectSingleNode($"//*[@Id='{idValue}']") is XmlElement idElement))
+            {
+                idElement = base.GetIdElement(document, idValue);
+            }
+
+            return idElement;
+        }
+    }
+}
---
+++ Projects/EMLabelCreator/EMLabelCreator/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EMLabelCreator")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EMLabelCreator")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5090ba5-bf1f-4e6d-ba90-4e888fbea3e1")]
